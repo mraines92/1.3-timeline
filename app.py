ed35facb14e1ceae5d9e316eb526e91a6ff727e7
Mason Raines <rainesmt92@gmail.com>
2019-01-09 22:15:10 -0500

    played with some CSS

--- app.py
+++ app.py
@@ -4,15 +4,22 @@ from flask import request
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+
 
 input = request.args.get('one')
 print(input)
+
+if input = 'one'
 
 # open file for reading, 'r'
 # file is saved to variable
 index_file = open('index.html', 'r')
 # read contents of the file
 my_html = index_file.read()
+
+my_value = ""
 
 if input:
     # add use input back into the html
@@ -21,4 +28,4 @@ if input:
 # close the file out when you're done
 index_file.close()
 
-return my_html
+print('my_html')
