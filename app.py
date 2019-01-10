864cd8357ffc5e47ef77179def2b727b1a519b55
Mason Raines <rainesmt92@gmail.com>
2019-01-09 20:55:53 -0500

    messed with some Python

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
 
-print('hello')
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+input = request.args.get('one')
+print(input)
+
+# open file for reading, 'r'
+# file is saved to variable
+index_file = open('index.html', 'r')
+# read contents of the file
+my_html = index_file.read()
+
+if input:
+    # add use input back into the html
+    my_html = my_html.replace("{{left}}", input)
+
+# close the file out when you're done
+index_file.close()
+
+return my_html
